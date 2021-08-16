3e03c3fbc22bfc973b85019e3ed44fdeb8c5a5ad
Ushnik Nath <63283081+SPUTnik-42@users.noreply.github.com>
2021-08-16 14:00:32 +0530

    timer change

--- public/js/main.js
+++ public/js/main.js
@@ -7,11 +7,6 @@ window.onload = function () {
 	startTimer(timer, display);
 };
 
-window.addEventListener('blur', () => {
-	document.getElementById("regForm").submit();
-});
-
-
 function startTimer(duration, display) {
 	var timer = duration, minutes, seconds;
 	setInterval(function () {
